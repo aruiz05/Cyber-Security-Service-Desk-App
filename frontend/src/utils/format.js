16873564d53daf0107e9804e8de6f655bdf0c247
Adan Ruiz <aruiz@mac.3520.dhcp.asu.edu>
2026-09-01 12:53:23 -0700

    formatting stuff

---
+++ frontend/src/utils/format.js
@@ -0,0 +1,19 @@
+export function formatDateTime(value, fallback = "Not available") {
+  if (!value) {
+    return fallback;
+  }
+
+  const date = new Date(value);
+
+  if (Number.isNaN(date.getTime())) {
+    return fallback;
+  }
+
+  return new Intl.DateTimeFormat(undefined, {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+    hour: "numeric",
+    minute: "2-digit",
+  }).format(date);
+}
